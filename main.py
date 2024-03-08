149c6618e66670e595dd754ec1a6c2e915c2b038
mwangi-george <mwangigeorge648@gmail.com>
2024-03-08 22:25:00 +0300

    root app

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # set up a route
+
+
 @app.get("/")
+async def root():
+    return {"message": "Hello World!!"}
+
+# on termial:
+# uvicorn main:app --port=3000
+# to allow live development, use the reload tag
+# uvicorn main:app --reload
