b984e915d7be131a65bbb4d9a1b3faf6853aea4f
mwangi-george <mwangigeorge648@gmail.com>
2024-03-08 22:56:24 +0300

    updates

--- main.py
+++ main.py
@@ -31,12 +31,10 @@ async def post():
 async def put():
     return {"message": "Hello from put route"}
 
-user_name = "John Doe"
-user_email = "john_doe@gmail.com"
 
 # Changing api param
 
 
 @app.get("/user", description="This is a description about users", deprecated=True)
 async def get_user_info(name: str, email: str):
-    return {"name": user_name, "email": user_email}
+    return {"name": name, "email": email}
