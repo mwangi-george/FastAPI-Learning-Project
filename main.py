38d0c7723f40c9a40b6a6ccb0644f60bc128d0e9
mwangi-george <mwangigeorge648@gmail.com>
2024-03-09 07:12:11 +0300

    updates

--- main.py
+++ main.py
@@ -47,7 +47,12 @@ async def list_items():
 # return a specific item, say by id 5
 # http://localhost:8000/items/5
 
+# the following shows a string
+
 
 @app.get("/items/{item_id}")
 async def get_items(item_id):
     return {"item_id": item_id}
+
+# http://localhost:8000/items/2000 returns
+# {"item_id":"2000"}
